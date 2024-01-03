17be63a6975aa86172fd32fae8aae7c204d0551c
Mateusz Brzozowski <01158596@pw.edu.pl>
2024-01-03 16:07:51 +0100

    add drawing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-const environment = new Environment(Constants.WIDTH, Constants.HEIGHT)
+const environment = new Environment(Constants.WIDTH, Constants.HEIGHT);
 const radar = new Radar(Constants.WIDTH / 4, Constants.HEIGHT / 4);
 const customEvents = new CustomEvents(environment);
 
 let img;
+let squares = [];
 
 function setup() {
     createCanvas(Constants.windowWidth, Constants.windowHeight).parent('canvas');
@@ -21,9 +22,25 @@ function _update() {
 
 function draw() {
     _update();
+
+    if (mouseIsPressed) {
+        let square = {
+            x: mouseX - 5,
+            y: mouseY - 5
+        };
+        squares.push(square);
+        // TU DODAĆ ŻEBY W TYM MIEJSCU BYŁY ZERA NA ENV
+    }
+
     environment.draw(img);
     image(img, 0, 0, Constants.windowWidth, Constants.windowHeight);
+
+    for (let i = 0; i < squares.length; i++) {
+        noStroke();
+        fill(200, 0, 100, 100);
+        rect(squares[i].x, squares[i].y, 10, 10);
+    }
     noStroke();
     fill(200, 0, 100, 100);
-    rect(mouseX, mouseY, 10, 10);
+    rect(mouseX - 5 , mouseY - 5, 10, 10);
 }
